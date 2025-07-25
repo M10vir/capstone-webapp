982ac8be02c9b9bbd56b3e26a5c8b71d78044d05
odl-user-1805854 <odl_user_1805854@sandboxailabs1012.onmicrosoft.com>
2025-07-25 01:41:02 +0000

    Automated push from approval workflow

---
+++ push_to_github.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+REPO_DIR="C:\LabFiles\Capstone-Project"
+BRANCH="main"
+COMMIT_MSG="Automated push from approval workflow"
+
+cd "$REPO_DIR" || exit
+
+git add .
+git commit -m "$COMMIT_MSG"
+git push origin "$BRANCH"
